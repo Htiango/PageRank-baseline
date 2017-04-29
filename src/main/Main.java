f45926598f68d936a8778a68402c1fe0226497ae
Htiango <hongty106@gmail.com>
2017-04-29 15:51:17 +0800

    update some class method

---
+++ src/main/Main.java
@@ -0,0 +1,19 @@
+/**
+ * @author hty
+ */
+package main;
+
+import pageRank.PageRank;;
+
+public class Main {
+
+	public static void main(String[] args){
+
+		System.out.println("Hello world!");
+		
+//		PageRank clas = new PageRank();
+		
+		
+	}
+
+}
